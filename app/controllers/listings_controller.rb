4a79518a46f26487daf15a358586183cfb99ad6e
karan619 <karan.valecha92@gmail.com>
2015-06-15 14:12:29 +0530

    Added user permissions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  before_action :check_user, only: [:edit, :update, :destroy]
   # GET /listings
   # GET /listings.json
   def index
@@ -72,4 +73,11 @@ class ListingsController < ApplicationController
     def listing_params
       params.require(:listing).permit(:name, :description, :price, :image)
     end
+
+    def check_user
+      if current_user != @listing.user
+        redirect_to root_url, alert: "Unauthorised access"
+      end
+    end
+
 end
