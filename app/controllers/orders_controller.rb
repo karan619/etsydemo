2e2ffc4dfc7b27d6d1b7ad51dba8ece77f64e817
karan619 <karan.valecha92@gmail.com>
2015-06-18 00:48:44 +0530

    Added integration for stripe charge

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,6 +17,40 @@ class OrdersController < ApplicationController
     @listing = Listing.find(params[:listing_id])
   end
 
+  def create
+    @order = Order.new(order_params)
+    @listing = Listing.find(params[:listing_id])
+    @seller = @listing.user
+
+    @order.listing_id = @listing.id
+    @order.buyer_id = current_user.id
+    @order.seller_id = @seller.id
+
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@listing.price * 100).floor,
+        currency: "INR",
+        card: token
+        )
+      flash[:notice] = "Thanks for ordering!"
+    rescue Stripe::CardError => e
+      flash[:danger] = e.message
+    end
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to root_url }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616151555) do
+ActiveRecord::Schema.define(version: 20150617170112) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
